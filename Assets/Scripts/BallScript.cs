d4d0a44eca3692b61ebd3c0a7af91222e1e3a9af
Evan Howard <esynclair@icloud.com>
2020-11-08 09:25:36 +0000

    I added scripts for the ball and player, I added an animation

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    public Rigidbody rb;
+    float clubForce = 5000f;
+    Vector3 dir;
+    public Transform target;
+
+    private void Start()
+    {
+        dir = target.transform.position - transform.position;
+        dir = dir.normalized;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            rb.AddForce(dir * clubForce);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public GameObject player;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            player.transform.Rotate(0f, 1f, 0f, Space.World);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            player.transform.Rotate(0f, -1f, 0f, Space.World);
+        }
+    }
+}
