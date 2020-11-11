f1dcac46edec90f77150588c9b86388e313f7178
Evan Howard <esynclair@icloud.com>
2020-11-11 18:46:13 +0000

    I added the powerbar

---
+++ Assets/Scripts/PowerBar.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerBar : MonoBehaviour
+{
+    public GameObject powerBarGO;
+    public Image PowerBarMask;
+    public float barChangeSpeed = 1;
+    float maxPowerBarValue = 100;
+    float currentPowerBarValue;
+    bool powerIsIncreasing;
+    bool PowerBarON;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentPowerBarValue = 0;
+        powerIsIncreasing = true;
+        PowerBarON = true;
+        StartCoroutine(UpdatePowerBar());
+    }
+
+    IEnumerator UpdatePowerBar()
+    {
+        while (PowerBarON)
+        {
+            if (!powerIsIncreasing)
+            {
+                currentPowerBarValue -= barChangeSpeed;
+                if (currentPowerBarValue <= 0)
+                {
+                    powerIsIncreasing = true;
+                }
+            }
+            if (powerIsIncreasing)
+            {
+                currentPowerBarValue += barChangeSpeed;
+                if (currentPowerBarValue >= maxPowerBarValue)
+                {
+                    powerIsIncreasing = false;
+                }
+            }
+
+            float fill = currentPowerBarValue / maxPowerBarValue;
+            PowerBarMask.fillAmount = fill;
+            yield return new WaitForSeconds(0.02f);
+
+            if (Input.touchCount > 0)
+            {
+                PowerBarON = false;
+                LaunchRocket();
+                StartCoroutine(TurnOffPowerBar());
+
+            }
+        }
+        yield return null;
+    }
+    IEnumerator TurnOffPowerBar()
+    {
+        yield return new WaitForSeconds(0.5f);
+        powerBarGO.SetActive(false);
+    }
+
+    public void LaunchRocket()
+    {
+        Debug.Log("RocketLaunched");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
